d7e837308f50382aef79063f42f459a346870cb9
vvk17 <v12345@mail.ru>
2023-03-26 20:52:50 +1000

    2nd commit

--- services/web-blog/main.go
+++ services/web-blog/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	app := fiber.New()
 	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World")
+		return c.SendString("Hello World, 1")
 	})
 	log.Fatal(app.Listen(":3069"))
 }
