87428ab33ce96f2a21eb5edf3776174474695c23
vvk17 <v12345@mail.ru>
2023-03-28 20:15:28 +1000

    7.3

--- services/web-blog/routes/routes.go
+++ services/web-blog/routes/routes.go
@@ -2,6 +2,10 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/vvk17/microservices-go/services/web-blog/validators"
+	"github.com/vvk17/microservices-go/services/web-blog/utilities"
+	"github.com/vvk17/microservices-go/services/web-blog/models"
+	"github.com/vvk17/microservices-go/services/web-blog/database"
 )
 
 func HelloMate (c *fiber.Ctx) error {
@@ -9,6 +13,33 @@ func HelloMate (c *fiber.Ctx) error {
 }
 
 func AddAuthor (c *fiber.Ctx) error {
+	response := utilities.GetBaseResponseObject()
+	postBody := &validators.AuthorAddPostBody{}
+	log.Println("pkg routes func AddAuthor postBody ", *postBody)
+
+	if err := c.BodyParser(postBody); err != nil {
+		response["error"] = err.Error()
+		log.Println("routes-AddAuthor BodyParser error", response)
+		return c.Status(fiber.StatusInternalServerError).JSON(response)
+	} else {
+		if err := validators.ValidateStruct(postBody); err != nil {
+			log.Println("routes-AddAuthor ValidateStruct error ", response)
+			return c.Status(fiber.StatusInternalServerError).JSON(response)
+		} else {
+			author := models.Authors{Title: postBody.Title}
+			if _, err := database.Database.Orm.Insert(&author); err != nil {
+				response["error"] = err.Error()
+				log.Println("routes-AddAuthor Orm.Insert error", response)
+				return c.Status(fiber.StatusInternalServerError).JSON(response)
+			} else {
+				response["message"] = "Author added successfully"
+				response["status"] = "OK"
+				log.Println("routes-AddAuthor Orm.Insert OK", response)
+				return c.Status(fiber.StatusCreated).JSON(response)
+			}
+		}
+	}
+
 	return c.SendString ("AddAuthor")
 }
 
