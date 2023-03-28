3eeb2bb92d32aae0c1fd0f6e791fd133bd59257c
vvk17 <v12345@mail.ru>
2023-03-28 11:52:12 +1000

    4.2

--- services/web-blog/main.go
+++ services/web-blog/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"github.com/beego/beego/v2/client/orm"
 	"github.com/gofiber/fiber/v2"
 	"github.com/vvk17/microservices-go/services/web-blog/database"
 	"github.com/vvk17/microservices-go/services/web-blog/models"
