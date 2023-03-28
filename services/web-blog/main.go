4fff9861390a1e2dab69346e9a00525d0b6f7629
vvk17 <v12345@mail.ru>
2023-03-28 11:45:50 +1000

    4

--- services/web-blog/main.go
+++ services/web-blog/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"github.com/gofiber/fiber/v2"
 	"github.com/vvk17/microservices-go/services/web-blog/database"
+	"github.com/vvk17/microservices-go/services/web-blog/models"
 )
 
 func main() {
@@ -12,5 +13,8 @@ func main() {
 		return c.SendString("Hello World, 1")
 	})
 	database.ConnectDB()
+	log.Print("ConnectDB - success")
+	orm.RegisterModel(new(models.Authors))
+	log.Print("orm.RegisterModel - success")
 	log.Fatal(app.Listen(":3069"))
 }
---
+++ services/web-blog/models/models.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Authors struct {
+
+	Id 			int			`json:"id"`
+	Title		string 	 	`json:"title"`
+	CreatedAt	time.Time	`orm:"auto_now_add;type(datetime)" json:"created_at"`
+	UpdatedAt	time.Time	`orm:"auo_now;type(datetime)" json:"updated_at"`
+}
